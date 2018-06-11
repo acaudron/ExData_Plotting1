906ee3b4e49b7ea9340248ec6c1cc00985e9996a
Adrien Caudron <adrien.caudron@laposte.net>
2018-06-11 17:30:29 +0200

    Add the download of data.

--- plot1.R
+++ plot1.R
@@ -1,6 +1,10 @@
 #read data
+if(!file.exists("household_power_consumption.txt")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
 options(stringsAsFactors = FALSE)
-data <- read.csv("../household_power_consumption.txt", sep = ";")
+data <- read.csv("household_power_consumption.txt", sep = ";")
 #select data by date
 days <- seq(as.Date("2007-02-01"), as.Date("2007-02-02"), "days")
 mydata <- data[as.Date(data$Date, "%d/%m/%Y") %in% days,]
